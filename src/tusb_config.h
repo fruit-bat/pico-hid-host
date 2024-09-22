f7958a46499d7da82880cfd5c59a0df4ad198297
Phil <pmscull@gmail.com>
2024-09-22 15:51:50 +0100

    WIP

--- src/tusb_config.h
+++ src/tusb_config.h
@@ -118,6 +118,7 @@
 // bit rate = 115200, 1 stop bit, no parity, 8 bit data width
 #define CFG_TUH_CDC_LINE_CODING_ON_ENUM   { 115200, CDC_LINE_CONDING_STOP_BITS_1, CDC_LINE_CODING_PARITY_NONE, 8 }
 
+#define __not_in_flash_func(X) X
 
 #ifdef __cplusplus
  }
